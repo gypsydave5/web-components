1e4e335d84d1ba46f9f09b32ea0f656aecda64c3
David Wickes <dave.wickes@gmail.com>
2021-04-02 22:33:24 +0100

    Svelte exported as Web Component / Custom Element
    - interaction through changing the props

--- svelte-wc/src/App.svelte
+++ svelte-wc/src/App.svelte
@@ -1,11 +1,21 @@
-<svelte:options tag="my-element" />
+<svelte:options tag={"my-element"} />
 <script lang="ts">
-	export let name: string;
+	import RedSquare from "./RedSquare.svelte";
+	export let name: string
+	export let hellos: string[] = ['a', 'b', 'c']
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h2>{hellos}</h2>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<!--	Have to use svelte components as web components when in svelte app-->
+	<red-square />
+	<ul>
+	{#each hellos as hello}
+		<li>{hello}</li>
+	{/each}
+	</ul>
 </main>
 
 <style>
---
+++ svelte-wc/src/RedSquare.svelte
@@ -0,0 +1,16 @@
+<svelte:options tag={"red-square"} />
+
+<script>
+    export let colour = '#F77'
+</script>
+
+<div class="square" style="background-color: {colour}">
+
+</div>
+
+<style>
+    .square {
+        width: 50px;
+        height: 50px;
+    }
+</style>
