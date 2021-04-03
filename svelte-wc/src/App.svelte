3460a2c073b396cb60b0720ae70dde8f23ed1800
David Wickes <dave.wickes@gmail.com>
2021-04-03 13:21:40 +0100

    Svelte WC - communicating out using DOM events

--- svelte-wc/src/App.svelte
+++ svelte-wc/src/App.svelte
@@ -1,16 +1,22 @@
 <svelte:options tag={"my-element"} />
 <script lang="ts">
-	import RedSquare from "./RedSquare.svelte";
 	export let name: string
 	export let hellos: string[] = ['a', 'b', 'c']
+
+	// communicating outside of the svelte app require emitting events using `this.dispatchEvent`, not the
+	// built in Svelte event dispatcher
+	function onClick() {
+		this.dispatchEvent(new CustomEvent('boo', {bubbles: true, composed: true, detail: {a: 1}}))
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<h2>{hellos}</h2>
+	<button on:click={onClick}>Inside App</button>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<!--	Have to use svelte components as web components when in svelte app-->
-	<red-square />
+	<!--	Have to use svelte components as web components when in svelte app compiling to web component-->
+	<red-square></red-square>
 	<ul>
 	{#each hellos as hello}
 		<li>{hello}</li>
